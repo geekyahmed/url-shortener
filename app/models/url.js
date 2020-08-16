dff179ceaeeac1ac754d9193f805777f16799da3
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-16 07:04:47 +0100

    add URL schema in models

--- app/models/url.js
+++ app/models/url.js
@@ -1,1 +1,15 @@
-const mongoose = require
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UrlSchema = new Schema({
+  link: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+module.exports = { Url: mongoose.model('url', UrlSchema) }
