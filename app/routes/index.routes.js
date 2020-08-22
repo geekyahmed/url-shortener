2368f88f1eff90daef50d02194d38f27ccd70f44
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-22 02:28:58 +0100

    separate route for shoretening urls

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -2,10 +2,9 @@ const express = require('express')
 const urlController = require('../controllers/url.controller')
 const api = express.Router()
 
-api
-  .route('/')
-  .get(urlController.index)
-  .post(urlController.generateShortUrl)
+api.route('/').get(urlController.index)
+
+api.route('/new').post(urlController.generateShortUrl)
 
 api.route('/:shortLink').get(urlController.redirectUrl)
 
